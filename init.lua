680b2946f2b9aa206381b2ab915e90155a334f61
Nick McAvoy <makeavoy@gmail.com>
2024-03-20 21:59:07 -0400

    feat: initial config

---
+++ init.lua
@@ -0,0 +1,34 @@
+
+-- bootstrap lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.opt.clipboard = "unnamedplus"
+
+require("lazy").setup({
+ 'tpope/vim-surround',
+ 'nvim-lua/plenary.nvim',
+ {'nvim-telescope/telescope.nvim',
+ keys={
+	 {
+		 "<leader>pf",
+		 function() require("telescope.builtin").find_files({cwd = require("lazy.core.config").options.root}) end,
+		 desc = "Find Plugin File",
+	 },
+ },
+ },
+ {'nvim-treesitter/nvim-treesitter', build=":TSUpdate"}
+})
+
+require "maps"
+
+print "nvim lua loaded"
