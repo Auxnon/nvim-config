af359e9eba0ad91a36cf2aa054cffa836cc112b1
Nick McAvoy <makeavoy@gmail.com>
2024-04-29 13:39:52 -0400

    feat: new formatter

--- init.lua
+++ init.lua
@@ -16,6 +16,11 @@ vim.opt.clipboard = "unnamedplus"
 -- end bootstrap
 
 vim.g.mapleader = " "
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.smartindent=true
+vim.bo.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- vim.opt.number=true
 -- vim.opt.relativenumber=true
@@ -86,6 +91,7 @@ require("lazy").setup({
     },
     lazy = false,
 },
+'mhartington/formatter.nvim',
 })
 
 
