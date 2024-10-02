4f1a9740eff14fe0324f529bb9d8fe54db0ab4cf
Nick 'MakeAvoy' McAvoy <makeavoy@gmail.com>
2024-10-02 00:30:37 -0400

    feat: replace shortcuts

--- lua/maps/init.lua
+++ lua/maps/init.lua
@@ -8,3 +8,13 @@ vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
 vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 vim.keymap.set("n", "<C-f>", vim.cmd.Format)
+vim.keymap.set("n", "<leader>sr", ":%s/<C-r>0//g<Left><Left>", {desc="Replace all instances of clipboard in text"})
+vim.keymap.set("v", "<leader>sr", "y:%s/<C-r>0//g<Left><Left>", {desc="Replace all instances of text selection"})
+vim.keymap.set("n", "<leader>sR", function()
+    local old_word = vim.fn.expand "<cword>"
+    local new_word = vim.fn.input("Replace " .. old_word .. " by? ", old_word)
+    -- Check if the new_word is different from the old_word and is not empty
+    if new_word ~= old_word and new_word ~= "" then
+      vim.cmd(":%s/\\<" .. old_word .. "\\>/" .. new_word .. "/g")
+    end
+end)
