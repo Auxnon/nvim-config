6953b00aa41626bea7674c6a6e1a2cfc1dd51bce
Nick McAvoy <makeavoy@gmail.com>
2024-10-16 23:20:45 -0400

    feat: formatted maps, manual vimsurround maps

--- lua/maps/init.lua
+++ lua/maps/init.lua
@@ -2,22 +2,46 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", "<Cmd>Oil --float .<CR>")
 
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+local opts = nil
+vim.keymap.set("n", "gd", function()
+	vim.lsp.buf.definition()
+end, opts)
 -- vim.keymap.set("n", "K", function() vim.lsp.inlay_hint.enable(true) end, opts)
-vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+vim.keymap.set("n", "K", function()
+	vim.lsp.buf.hover()
+end, opts)
+vim.keymap.set("n", "<leader>r", function()
+	vim.lsp.buf.rename()
+end, opts)
+vim.keymap.set("n", "<leader>vws", function()
+	vim.lsp.buf.workspace_symbol()
+end, opts)
+vim.keymap.set("n", "<leader>vd", function()
+	vim.diagnostic.open_float()
+end, opts)
 vim.keymap.set("n", "<C-f>", vim.cmd.Format)
-vim.keymap.set("n", "<leader>sr", ":%s/<C-r>0//g<Left><Left>", {desc="Replace all instances of clipboard in text"})
-vim.keymap.set("v", "<leader>sr", "y:%s/<C-r>0//g<Left><Left>", {desc="Replace all instances of text selection"})
+vim.keymap.set("n", "<leader>sr", ":%s/<C-r>0//g<Left><Left>", { desc = "Replace all instances of clipboard in text" })
+vim.keymap.set("v", "<leader>sr", "y:%s/<C-r>0//g<Left><Left>", { desc = "Replace all instances of text selection" })
 vim.keymap.set("n", "<leader>sR", function()
-    local old_word = vim.fn.expand "<cword>"
-    local new_word = vim.fn.input("Replace " .. old_word .. " by? ", old_word)
-    -- Check if the new_word is different from the old_word and is not empty
-    if new_word ~= old_word and new_word ~= "" then
-      vim.cmd(":%s/\\<" .. old_word .. "\\>/" .. new_word .. "/g")
-    end
+	local old_word = vim.fn.expand("<cword>")
+	local new_word = vim.fn.input("Replace " .. old_word .. " by? ", old_word)
+	-- Check if the new_word is different from the old_word and is not empty
+	if new_word ~= old_word and new_word ~= "" then
+		vim.cmd(":%s/\\<" .. old_word .. "\\>/" .. new_word .. "/g")
+	end
 end)
-vim.keymap.set("n", "<C-s>", '<Cmd>:Format<CR><Cmd>:w<CR>')
--- require('leap').create_default_mappings()
+vim.keymap.set("n", "<C-s>", "<Cmd>:Format<CR><Cmd>:w<CR>")
+require("leap").create_default_mappings()
+require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+
+local k = vim.keymap
+k.set("n", "ds", "<Plug>Dsurround")
+k.set("n", "cs", "<Plug>Csurround")
+k.set("n", "cS", "<Plug>CSurround")
+k.set("n", "ys", "<Plug>Ysurround")
+k.set("n", "yS", "<Plug>YSurround")
+k.set("n", "yss", "<Plug>Yssurround")
+k.set("n", "ySs", "<Plug>YSsurround")
+k.set("n", "ySS", "<Plug>YSsurround")
+k.set("x", "S", "<Plug>VSurround")
+k.set("x", "gS", "<Plug>VgSurround")
