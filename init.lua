32b2fe6a41391d32fbebf32c61ff2e49736db8a0
Nick MakeAvoy <makeavoy@gmail.com>
2024-10-21 14:31:37 -0400

    feat: line, theme, oil bind changes

--- init.lua
+++ init.lua
@@ -114,8 +114,25 @@ require("lazy").setup({
 		---@module 'oil'
 		---@type oil.SetupOpts
 		opts = {
+			use_default_keymaps = false,
 			keymaps = {
 				["q"] = "actions.close",
+				--  ["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+				["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+				["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = "actions.close",
+				-- ["<C-l>"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				-- ["`"] = "actions.cd",
+				-- ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+				-- ["gs"] = "actions.change_sort",
+				-- ["gx"] = "actions.open_external",
+				-- ["g."] = "actions.toggle_hidden",
+				-- ["g\\"] = "actions.toggle_trash",
 			},
 		},
 		-- Optional dependencies
@@ -124,6 +141,27 @@ require("lazy").setup({
 	},
 	"ggandor/leap.nvim",
 	"wellle/targets.vim",
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				-- Recommended - see "Configuring" below for more config options
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
+				-- theme = { variant = "light", colors = { bg = "#ffffff" } },
+			})
+			vim.cmd("colorscheme cyberdream") -- set the colorscheme
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 })
 
 require("maps")
